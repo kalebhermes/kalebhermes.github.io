272981121a4dcf3ff0827c89c7791cd25c25e42d
Kaleb Hermes <Kaleb.Hermes@sizmek.com>
2020-02-07 00:52:08 -0500

    A ton of changes
    * Added skelleton.css to prettify the layout of the inputs
    * Increased width to 375px from 350 to accomidate wider cost values
    * Added Source
    * Added remove row functionality

--- js/output.js
+++ js/output.js
@@ -1,13 +1,15 @@
-var parentStart = '<div style="width:350px;">';
+var parentStart = '<div style="width:375px;">';
 var parentEnd = '</div>';
 
 function update(thisMagicItem) {
 	var output = parentStart;
 	output += getHeaderBar(thisMagicItem);
-	output += parentEnd;
+	
 	if(thisMagicItem.levelBonusCostArray.length > 0) output += getLevelBonusCost(thisMagicItem);
 	if(thisMagicItem.propertiesArray.length > 0) output += getProperties(thisMagicItem);
 	if(thisMagicItem.powerArray.length > 0) output += getPowers(thisMagicItem);
+	if(thisMagicItem.source != undefined) output += getSource(thisMagicItem);
+	output += parentEnd;
 	document.getElementById('output').innerHTML = output;
 	document.getElementById('htmlSource').value = output;
 }
@@ -17,7 +19,7 @@ function getHeaderBar(thisMagicItem) {
 			if(thisMagicItem.name) template += '<div style="font-weight:bold;float:left;font-family:Mentor-Sans;font-size:20px;">' + thisMagicItem.name + '</div>';
 			if(thisMagicItem.levelModifier) template += '<div style="float:right;font-family:Mentor-Sans;font-weight:bold;font-size:14px;padding:5px 10px 0px 0px;">Level ' + thisMagicItem.levelModifier + '+</div>';
 		template += '</div>';
-		if(thisMagicItem.flavorText) template += '<div>' + '<div style="background-color:#DDDDCC;background-image:url(\'http://omnichron.net/external/op/src/bg_350.jpg\');font-style:italic;padding: 2px 0px 0px 10px;margin: 2px 0px 0px 0px;font-family:Mentor;">' + thisMagicItem.flavorText + '</div>' + '</div>';
+		if(thisMagicItem.flavorText) template += '<div>' + '<div style="background-color:#DDDDCC;background-image:url(images/gradient.jpg);font-style:italic;padding: 2px 0px 0px 10px;margin: 2px 0px 0px 0px;font-family:Mentor;">' + thisMagicItem.flavorText + '</div>' + '</div>';
 
 	return template;
 }
@@ -40,7 +42,7 @@ function howManyItemsInAColumn(numberOfItems) {
 }
 
 function getLevelBonusCost(thisMagicItem) {
-	var template = '<div style="display:inline-block;width:350px;margin:5px 0px 0px 0px;font-family:Mentor-Sans;"><table style="border-style:none;border-collapse:collapse;margin:0px 0px 0px 10px;width:165px;display:inline-block;">';
+	var template = '<div style="display:inline-block;width:375px;margin:5px 0px 0px 0px;font-family:Mentor-Sans;"><table style="border-style:none;border-collapse:collapse;margin:0px 0px 0px 10px;width:165px;display:inline-block;">';
 	var templateEnd = '</div>';
 	var tableEnd = '</table>';
 	var divEnd = '</div>';
@@ -48,7 +50,7 @@ function getLevelBonusCost(thisMagicItem) {
 
 	// All items will have at least one table.
 	for(var x = 0; x < numColumns[0]; x++) {
-		template += '<tr><td style="border-style:none;padding:0px 0px 0px 0px;width:55px;">Lvl ' + thisMagicItem.levelBonusCostArray[x].level + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:25px">+' + thisMagicItem.levelBonusCostArray[x].bonus + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:85px;text-align:right;">' + thisMagicItem.levelBonusCostArray[x].cost + ' gp</td></tr>';
+		template += '<tr><td style="border-style:none;padding:0px 0px 0px 0px;width:55px;">Lvl ' + (thisMagicItem.levelBonusCostArray[x].level || '') + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:25px">+' + (thisMagicItem.levelBonusCostArray[x].bonus || '') + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:95px;text-align:right;">' + (thisMagicItem.levelBonusCostArray[x].cost || '') + ' gp</td></tr>';
 	}
 
 	if(numColumns[1] != 0) {
@@ -56,7 +58,7 @@ function getLevelBonusCost(thisMagicItem) {
 		template += '<table style="border-style:none;border-collapse:collapse;margin:0px 0px 0px 10px;width:165px;display:inline-block;float:right;">';
 		for(var x = 0; x < numColumns[1]; x++) {
 			var index = x + numColumns[0];
-			template += '<tr><td style="border-style:none;padding:0px 0px 0px 0px;width:55px;">Lvl ' + thisMagicItem.levelBonusCostArray[index].level + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:25px">+' + thisMagicItem.levelBonusCostArray[index].bonus + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:85px;text-align:right;">' + thisMagicItem.levelBonusCostArray[index].cost + ' gp</td></tr>';	
+			template += '<tr><td style="border-style:none;padding:0px 0px 0px 0px;width:55px;">Lvl ' + (thisMagicItem.levelBonusCostArray[index].level || '') + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:25px">+' + (thisMagicItem.levelBonusCostArray[index].bonus || '') + '</td><td style="border-style:none;padding:0px 0px 0px 0px;width:102px;text-align:right;">' + (thisMagicItem.levelBonusCostArray[index].cost || '') + ' gp</td></tr>';
 		}
 	}
 
@@ -69,7 +71,7 @@ function getProperties(thisMagicItem) {
 	var template = '<div style="display: inline-block;margin: 0px 0px 0px 10px;">';
 	
 	for(var x = 0; x < thisMagicItem.propertiesArray.length; x++) {
-		template += '<div style="margin: 0px 0px 2px 0px;font-family:Mentor-Sans"><b style="display: inline-block;">' + thisMagicItem.propertiesArray[x].label + ':</b> ' + thisMagicItem.propertiesArray[x].value + '</div>';
+		template += '<div style="margin: 0px 0px 2px 0px;font-family:Mentor-Sans"><b style="display: inline-block;">' + (thisMagicItem.propertiesArray[x].label || '') + ':</b> ' + (thisMagicItem.propertiesArray[x].value || '') + '</div>';
 	}
 
 	template += '</div>';
@@ -77,14 +79,20 @@ function getProperties(thisMagicItem) {
 }
 
 function getPowers(thisMagicItem) {
-	var template = '<div style="background-color:#DDDDCC;background-image:url(\'http://omnichron.net/external/op/src/bg_350.jpg\');padding: 2px 0px 0px 10px;">';
+	var template = '<div style="background-color:#DDDDCC;background-image:url(images/gradient.jpg);padding: 2px 0px 0px 10px;">';
 
 	for(var x = 0; x < thisMagicItem.powerArray.length; x++) {
 		template += '<div style="text-indent: -13px;margin: 0px 0px 5px 13px;font-family:Mentor-Sans;"><b style="font-weight:bold;">Power (' + thisMagicItem.powerArray[x].frequency;
 		if(thisMagicItem.powerArray[x].types != undefined && thisMagicItem.powerArray[x].types != '') template += ' ♦ ' + thisMagicItem.powerArray[x].types;
-		template +=  '):</b> ' + thisMagicItem.powerArray[x].description + '</div>';
+		template +=  '):</b> ' + (thisMagicItem.powerArray[x].description || '') + '</div>';
 	}
 
 	template += '</div>';
 	return template;	
+}
+
+function getSource(thisMagicItem) {
+	var template = '<div style="font-size: 12px;color: rgb(0, 0, 0, .75);font-style: italic;font-family: Mentor;margin: -5px 0px 5px 0px;letter-spacing: .25px;">' + (thisMagicItem.source || '') + '</div>';
+
+	return template;
 }
